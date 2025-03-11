a3bbd12397dadd40fe5d1e0cbf35eb4fcc4e2c33
Ayush-1712 <ayush1712sharma@gmail.com>
2025-03-11 13:57:25 +0530

    ssh key added to ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,12 @@
+resource "aws_key_pair" "demo_key" {
+  key_name   = "demo-key"
+  public_key = file("~/.ssh/demo-key.pub")
+}
+
 resource "aws_instance" "test_instance" {
   ami           = "ami-05c179eced2eb9b5b"  
   instance_type = "t2.micro"
+  key_name = aws_key_pair.demo_key.key_name
 
   tags = {
     Name = "test-instance"
